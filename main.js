7f8495dd3e22b582dcaa5957534082fd44de37eb
TriPhan12 <pmtri96@gmail.com>
2019-03-03 15:16:16 +0700

    git clone

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
+//var dt = require('./myfirstmodule');
 
 //create server object
 http.createServer(function (req,res){
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hello World!');  //Write a response to the client
-    res.write("The date and time now are: "+ dt.myDateTime());
-    res.end();
+    //res.write("The date and time now are: "+ dt.myDateTime());
+    res.end('Hello, World!');
 }).listen(8080);    //Thr server objecr listens on port 8080
 //log.respone("Go to htttps://localhost:8080");                                                                                     
